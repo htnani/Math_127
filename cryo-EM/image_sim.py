d9c61e3036976a7e8c5d3aefd24a172a14bdab61
Ethan Chen <boycyf1996@163.com>
2017-11-17 09:12:51 -0800

    Muiltiprocessing

---
+++ cryo-EM/image_sim.py
@@ -0,0 +1,47 @@
+from scipy.interpolate import RegularGridInterpolator as RGI
+from matplotlib import pyplot as plt
+import numpy as np 
+
+def project_fst(R, mol):
+	'''
+	mol is an NXNXN array that contains the electron density of the protein thing
+	R is the rotation matrix that symbolizes the viewing direction of the microscope
+	'''
+	
+	#print('Now running 3D fourier Transform ...')
+	N = len(mol)
+	rho_hat = np.fft.fftshift(np.fft.fftn(mol))
+	wx, wy, wz = np.meshgrid(np.arange(-(N-1)/2, (N-1)/2+1, dtype= int), np.arange(-(N-1)/2, (N-1)/2+1, dtype= int), np.arange(-(N-1)/2, (N-1)/2+1, dtype= int))
+	rho_hat = ((-1)**np.abs(wx+wy+wz)/(N**3)*rho_hat)
+
+	#print('Now running Linear Interpolation ...')
+	N_range = np.arange(-(N-1)/2, (N-1)/2+1)
+	rho_hat = RGI((N_range,N_range,N_range), rho_hat, bounds_error= False, fill_value=0)
+
+	### make 2D mesh that takes slice of rho_hat which NxNx3 grid 
+	#print('Creating 2D Sampling grid ...')
+	eta_x, eta_y = np.meshgrid(np.arange(-(N-1)/2, (N-1)/2+1, dtype= int), np.arange(-(N-1)/2, (N-1)/2+1, dtype= int), indexing= "ij")
+	eta_x= eta_x[...,np.newaxis]
+	eta_y= eta_y[...,np.newaxis]
+
+	R= np.transpose(R)
+	a= R[0]
+	b= R[1]
+	grid = eta_x*a + eta_y*b
+	sample_grid = rho_hat(grid)
+
+	em_slice = (-1)**np.abs(eta_x[...,0]+eta_y[...,0])*(N**2)*sample_grid
+
+	em_slice = np.fft.ifftn(np.fft.ifftshift(em_slice))
+
+	return np.real(em_slice)
+
+def get_rotation_matrix():
+    a = 2*np.random.rand(3)-1
+    a /= np.linalg.norm(a)
+    b = np.cross(a, 2*np.random.rand(3)-1)
+    b /= np.linalg.norm(b)
+    c = np.cross(a, b)
+    c /= np.linalg.norm(c)
+
+    return np.transpose(np.array([a, b, c]))
---
+++ cryo-EM/test_image_sim.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python3
+import mrcfile
+from scipy.interpolate import RegularGridInterpolator as RGI
+import sys
+from matplotlib import pyplot as plt
+import image_sim as SIM
+import multiprocessing
+from functools import partial
+
+##########################
+#####input
+#input_file=mrcfile.open('emd_8116.map')
+input_file=mrcfile.open('zika_153.mrc')
+rotaion_num = int(20)
+
+#####
+print ('Now generating random rotation matrix ...')
+Rs = [SIM.get_rotation_matrix() for i in range(rotaion_num)]
+print ('Now running project_fst ...')
+rho = input_file.data
+n = 0
+
+#multi processing
+cores = multiprocessing.cpu_count()
+pool = multiprocessing.Pool(processes=cores)
+fit_rho = partial(SIM.project_fst, mol = rho)
+
+for image in pool.imap(fit_rho, Rs):
+	plt.imshow(image)
+	plt.savefig(str(n)+'.png')
+	n += 1
+
+'''
+#single processing
+for R in Rs:
+	image = SIM.project_fst(R, rho)
+	plt.imshow(image)
+	plt.savefig(str(n)+'.png')
+	n += 1
+	'''
+
+sys.exit()
+
